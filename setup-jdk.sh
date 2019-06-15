9d481070666b453b0ce21c7b4943f039d08ef454
smac89 <nobleman.code@gmx.com>
2019-06-15 15:42:40 -0600

    Keep using the jdk_switcher from git

--- setup-jdk.sh
+++ setup-jdk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "${TRAVIS_OS_NAME}" == 'linux' ]; then
-  # if ! command -v jdk_switcher; then
-  #   git clone https://github.com/michaelklishin/jdk_switcher.git
-  #   source jdk_switcher/jdk_switcher.sh
-  # fi
+  if ! command -v jdk_switcher; then
+    git clone https://github.com/michaelklishin/jdk_switcher.git
+    source jdk_switcher/jdk_switcher.sh
+  fi
   jdk_switcher use openjdk8
 else
   export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
