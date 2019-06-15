5fd0271adc9ead63b5177464e9cf5925a06d7626
smac89 <nobleman.code@gmx.com>
2019-06-15 16:22:29 -0600

    document patch script

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Applies our changes to the source code of the java-cef repo.
+# Useful for debugging
+# You can generate the patch using the command (Assuming this build repo is located in the same folder as src):
+# `cd src && git diff --no-color > ../java-cef-build/patch/docs.patch`
+
 cdir=$(pwd)
 pushd $1
 git apply --verbose "$cdir/patch/docs.patch"
