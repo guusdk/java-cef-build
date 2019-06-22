5565574ed7c01ca3d14e8ce8fbece8d098ad350e
smac89 <nobleman.code@gmx.com>
2019-06-22 13:35:45 -0600

    [skip travis] disable running gradle as a daemon

--- setup-deploy.sh
+++ setup-deploy.sh
@@ -1,7 +1,7 @@
 # Build the jars and native libraries
-./gradlew -DBIN_ARTIFACT="$1" --info jcef
+./gradlew -DBIN_ARTIFACT="$1" --info --no-daemon jcef
 
 # Only build the pages if this is linux64
 if [[ $ARCH_DIST == 'linux64' ]]; then
-  ./gradlew -DBIN_ARTIFACT="$1" --info installDocsDist
+  ./gradlew -DBIN_ARTIFACT="$1" --info --no-daemon installDocsDist
 fi
