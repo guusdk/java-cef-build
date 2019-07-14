a68cff8e03e900d69e5aa5e9721c3b635f3563a0
smac89 <nobleman.code@gmx.com>
2019-07-14 16:04:56 -0600

    Revert back to using clang for xcode

--- build.sh
+++ build.sh
@@ -5,18 +5,13 @@ cd $JCEF_DIR/jcef_build
 
 # Generate build script with cmake
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then
-  cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Release -DPROJECT_ARCH="x86_64" ../
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPROJECT_ARCH="x86_64" ../
 else
   cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX ../
 fi
 
 # build the library
-if [ "$TRAVIS_OS_NAME" == 'osx' ]; then
-  # https://www.manpagez.com/man/1/xcodebuild/
-  xcodebuild -project jcef.xcodeproj -configuration Release
-else
-  ninja
-fi
+ninja || exit 1
 
 cd ../tools
 
