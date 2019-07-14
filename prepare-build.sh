5071d4c24e05d7ea18fcda2343144842c4c7cf51
smac89 <nobleman.code@gmx.com>
2019-07-14 14:03:52 -0600

    Switch to bitbucket builds and pushing to see if anything breaks

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/chromiumembedded/java-cef.git $JCEF_DIR
+git clone https://bitbucket.org/chromiumembedded/java-cef.git $JCEF_DIR
 ./apply-patches.sh $JCEF_DIR
 
 # jdk stuff taken care of in docker image
