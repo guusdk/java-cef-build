be35d181a0ec998345fcc712421de55171660ddd
smac89 <nobleman.code@gmx.com>
2019-07-14 14:42:07 -0600

    Going to attempt to use xcode to build osx

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ cd $JCEF_DIR/jcef_build
 
 # Generate build script with cmake
 if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then
-  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPROJECT_ARCH="x86_64" ../
+  cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Release -DPROJECT_ARCH="x86_64" ../
 else
   cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX ../
 fi
 
 # build the library
-ninja
+if [ "$TRAVIS_OS_NAME" == 'osx' ]; then
+  xcodebuild  -list -project jcef.xcodeproj
+else
+  ninja
+fi
 
 cd ../tools
 
