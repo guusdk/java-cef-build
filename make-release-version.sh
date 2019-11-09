a7c9217a55c742fd02d27fb0d05cba986a6ef878
smac89 <nobleman.code@gmx.com>
2019-11-09 16:50:07 -0600

    Match more strictly

--- make-release-version.sh
+++ make-release-version.sh
@@ -2,7 +2,7 @@
 
 {
 	# Pattern taken from https://bitbucket.org/chromiumembedded/cef/issues/2596/improve-cef-version-number-format#comment-50679036
-	export CEF_VERSION=$(perl -n -e  '/set\s*\(CEF_VERSION\s+"((?:\d+\.?){3}\+g\w+\+chromium-(?:\d+\.?){4})"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
+	export CEF_VERSION=$(perl -n -e  '/^\s*set\s*\(CEF_VERSION\s+"((?:\d+\.?){3}\+g\w+\+chromium-(?:\d+\.?){4})"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
 	echo -e '\n\nChanges'
 
 	echo "git log --pretty=format:'%t - %s <%aN>' <commit1>...<commit2>"
