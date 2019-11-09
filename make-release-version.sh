68e023e3bc0d805024fe25cb27250eea916d2c7b
smac89 <nobleman.code@gmx.com>
2019-11-08 18:35:46 -0600

    redirect extra output to dev null

--- make-release-version.sh
+++ make-release-version.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# $1 is the directory for jcef sources
-pushd $1 2>&1 > /dev/null
-git stash 2>&1 > /dev/null
-git pull
-popd 2>&1 > /dev/null
-export CEF_VERSION=$(perl -n -e  '/set\s*\(CEF_VERSION\s+"(.+)"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
-echo -e '\n\nChanges'
+{
+	# $1 is the directory for jcef sources
+	pushd $1 2>&1 > /dev/null
+	git stash 2>&1 > /dev/null
+	git pull
+	popd 2>&1 > /dev/null
+	export CEF_VERSION=$(perl -n -e  '/set\s*\(CEF_VERSION\s+"(.+)"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
+	echo -e '\n\nChanges'
 
-echo "git log --pretty=format:'%t - %s <%aN>' <commit1>...<commit2>"
+	echo "git log --pretty=format:'%t - %s <%aN>' <commit1>...<commit2>"
 
-pushd $1 2>&1 > /dev/null
-git stash pop 2>&1 > /dev/null
-popd 2>&1 > /dev/null
+	pushd $1 2>&1 > /dev/null
+	git stash pop 2>&1 > /dev/null
+	popd 2>&1 > /dev/null
 
-if [ -z $CEF_VERSION ]; then
-  echo "Failed to retrieve cef version"
-  exit 1
-fi
+	if [ -z $CEF_VERSION ]; then
+	  echo "Failed to retrieve cef version"
+	  exit 1
+	fi
+} > /dev/null
 
-echo "CEF_VERSION=$CEF_VERSION"
+echo "$CEF_VERSION"
 
 # git tag --annotate "$CEF_VERSION" --file <(cat << 'EOF'
 #
