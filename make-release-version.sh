e732ebec2ff3358071c65860a59f89a22a3c386f
smac89 <nobleman.code@gmx.com>
2019-11-09 03:28:39 -0600

    assume jcef repo is up-to-date and read the latest version

--- make-release-version.sh
+++ make-release-version.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
 {
-	# $1 is the directory for jcef sources
-	pushd $1 2>&1 > /dev/null
-	git stash 2>&1 > /dev/null
-	git pull
-	popd 2>&1 > /dev/null
 	export CEF_VERSION=$(perl -n -e  '/set\s*\(CEF_VERSION\s+"(.+)"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
 	echo -e '\n\nChanges'
 
 	echo "git log --pretty=format:'%t - %s <%aN>' <commit1>...<commit2>"
-
-	pushd $1 2>&1 > /dev/null
-	git stash pop 2>&1 > /dev/null
-	popd 2>&1 > /dev/null
 
 	if [ -z $CEF_VERSION ]; then
 	  echo "Failed to retrieve cef version"
@@ -22,15 +13,3 @@
 } > /dev/null
 
 echo "$CEF_VERSION"
-
-# git tag --annotate "$CEF_VERSION" --file <(cat << 'EOF'
-#
-# - Builds for linux and mac are working
-# - Deploy for linux working as well
-# EOF
-# )
-
-# git tag --delete "$CEF_VERSION"
-# git push origin --delete "$CEF_VERSION"
-
-# git push origin "$CEF_VERSION"
