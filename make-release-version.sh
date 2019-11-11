296da5ad840c70b7a65e80a5b3ddeeecc5fe8222
smac89 <nobleman.code@gmx.com>
2019-11-10 18:06:34 -0600

    [ci skip] Change the syntax for generating logs

--- make-release-version.sh
+++ make-release-version.sh
@@ -5,7 +5,7 @@
 	export CEF_VERSION=$(perl -n -e  '/^\s*set\s*\(CEF_VERSION\s+"((?:\d+\.?){3}\+g\w+\+chromium-(?:\d+\.?){4})"\s*\)/i && print "$1"' "$1/CMakeLists.txt")
 	echo -e '\n\nChanges'
 
-	echo "git log --pretty=format:'%t - %s <%aN>' <commit1>...<commit2>"
+	echo "git log --pretty=format:'%h - %s <%aN>' <after_commit>...<latest_commit>"
 
 	if [ -z $CEF_VERSION ]; then
 	  echo "Failed to retrieve cef version"
