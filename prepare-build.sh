c9c44a5610a0e3abff64b3351b4055c0037e5792
smac89 <nobleman.code@gmx.com>
2020-01-28 17:21:27 -0600

    Remove env

--- prepare-build.sh
+++ prepare-build.sh
@@ -2,6 +2,6 @@ git clone https://github.com/chromiumembedded/java-cef.git $JCEF_DIR
 ./apply-patches.sh $JCEF_DIR
 
 # jdk stuff taken care of in docker image
-if [ $ARCH_DIST != 'linux32' ]; then
+if [ ! -z "$ARCH_DIST" ] && [ $ARCH_DIST != 'linux32' ]; then
   source ./setup-jdk.sh
 fi
