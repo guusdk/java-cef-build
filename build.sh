c709f2852a376e829dfb88a483e8e7c85f8ea3cf
smac89 <nobleman.code@gmx.com>
2020-02-19 20:41:52 -0600

    random commit to force a build

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 mkdir -p $JCEF_DIR/jcef_build
 cd $JCEF_DIR/jcef_build
 
-# Generate build script with cmake
-if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then
+# generate build script with cmake
+if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
   cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPROJECT_ARCH="x86_64" ../
 else
   cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX ../
@@ -17,11 +17,10 @@ set +x
 
 cd ../tools
 
-# Compile the java source on Linux
-if [[ "${TRAVIS_OS_NAME}" == 'linux' ]]; then
+# compile the java source on Linux
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
   ./compile.sh $ARCH_DIST
 fi
 
-# Generate docs, copy libraries and jars to binary_distrib folder, etc
+# generate docs, copy libraries and jars to binary_distrib folder, etc
 ./make_distrib.sh $ARCH_DIST
-
